522784a3672aea22be0af70c2d0b4f6646e8a38e
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-01 11:42:07 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let message 
+  if (distance <= 400) {
+    message = 'This one is on me!';
+  }
+  else if (distance <= 2000) {
+    message = 'That will be twenty bucks.';
+  }
+  else if (distance <= 2500) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let cityMessage;
+  city === 'NYC' ? (cityMessage = 'Ok, sounds good.') : (cityMessage = 'No go.')
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let tipMessage;
+  switch (tip) {
+    case 'generous':
+      tipMessage = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipMessage = 'Thank you.';
+      break;
+    default:
+      tipMessage = 'Bye.';
+  }
+  return tipMessage;
 }
